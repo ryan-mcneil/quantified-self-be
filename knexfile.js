b84dc9a0c58a034b0240a865da0d03d7feef70f9
Ryan McNeil <ryan.mcneil288@gmail.com>
2019-01-29 17:22:22 -0700

    Update knexfile.js
    add production db

--- knexfile.js
+++ knexfile.js
@@ -14,4 +14,13 @@ module.exports = {
     useNullAsDefault: true,
   },
 
+  production: {
+    client: 'pg',
+    debug: true,
+    connection: 'postgres://febymtnmqqdgik:201c5d9d7163fa8caf89eefed55f3e3454628c28551b12a868addfb5187a3237@ec2-54-243-228-140.compute-1.amazonaws.com:5432/d1sgc6nfffhvn',
+    migrations: {
+      tableName: 'migrations'
+    }
+  }
+
 };
