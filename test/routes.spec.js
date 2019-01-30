3b0ada8e3e93b9c41a4c8fd7ddfd9b2e1eb87508
Ryan McNeil <ryan.mcneil288@gmail.com>
2019-01-30 14:33:03 -0700

    Add test for GET foods

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -30,3 +30,39 @@ describe('Client Routes', () => {
     });
   });
 })
+
+describe('API Routes', () => {
+  before((done) => {
+    database.migrate.latest()
+      .then(() => done())
+      .catch(error => {
+        throw error;
+      });
+  });
+
+  beforeEach((done) => {
+    database.seed.run()
+      .then(() => done())
+      .catch(error => {
+        throw error;
+      });
+  });
+
+  describe('GET /api/v1/foods', () => {
+    it('should return all of the foods', done => {
+      chai.request(server)
+        .get('/api/v1/foods')
+        .end((err, response) => {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.be.a('array');
+          response.body.length.should.equal(3);
+          response.body[0].should.have.property('name');
+          response.body[0].title.should.equal('Steak');
+          response.body[0].should.have.property('calories');
+          response.body[0].author.should.equal('7');
+          done();
+      });
+    });
+  });
+});
