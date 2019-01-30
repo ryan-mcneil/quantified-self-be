c4d23774ac7b3ccd1b57a1da09edcc1797ac94f1
Michael Gatewood <michael@mngatewood.com>
2019-01-29 17:55:27 -0700

    Update production connection with DATABASE_URL env variable

--- knexfile.js
+++ knexfile.js
@@ -17,7 +17,7 @@ module.exports = {
   production: {
     client: 'pg',
     debug: true,
-    connection: 'postgres://febymtnmqqdgik:201c5d9d7163fa8caf89eefed55f3e3454628c28551b12a868addfb5187a3237@ec2-54-243-228-140.compute-1.amazonaws.com:5432/d1sgc6nfffhvn',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
     migrations: {
       directory: './db/migrations'
     }
