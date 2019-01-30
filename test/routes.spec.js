bc95598023d73a80fa11b2fdcef4de94e7be0c25
Ryan McNeil <ryan.mcneil288@gmail.com>
2019-01-30 16:32:31 -0700

    Write test for GET food
    test for food show page

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -66,4 +66,21 @@ describe('API Routes', () => {
       });
     });
   });
+
+  describe('GET /api/v1/foods/:id', () => {
+    it('should return a specific food', done => {
+      chai.request(server)
+        .get('/api/v1/foods/1')
+        .end((err, response) => {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.be.a('object');
+          response.body.should.have.property('name');
+          response.body.name.should.equal('Steak');
+          response.body.should.have.property('calories');
+          response.body.calories.should.equal(7);
+          done();
+      });
+    });
+  });
 });
