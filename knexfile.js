adb93aa3d4501effe91666c47cb81f94cc50620a
Ryan McNeil <ryan.mcneil288@gmail.com>
2019-01-29 17:29:36 -0700

    fix migration directory

--- knexfile.js
+++ knexfile.js
@@ -19,7 +19,7 @@ module.exports = {
     debug: true,
     connection: 'postgres://febymtnmqqdgik:201c5d9d7163fa8caf89eefed55f3e3454628c28551b12a868addfb5187a3237@ec2-54-243-228-140.compute-1.amazonaws.com:5432/d1sgc6nfffhvn',
     migrations: {
-      tableName: 'migrations'
+      directory: './db/migrations'
     }
   }
 
