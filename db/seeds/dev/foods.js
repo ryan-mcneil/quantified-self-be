a1549fa84d410b4dd4152a616ee19a91ee2952b7
Ryan McNeil <ryan.mcneil288@gmail.com>
2019-02-03 14:37:03 -0700

    create meals seed file
    create 2 days worth of meals, and add extra items to foods

--- db/seeds/dev/foods.js
+++ db/seeds/dev/foods.js
@@ -4,13 +4,20 @@ exports.seed = function(knex, Promise) {
     .then(() => {
       return Promise.all([
 
-        knex('foods').insert([{id: 1, name: 'Steak', calories: 7},
-                              {id: 2, name: 'Carrots', calories: 1000},
-                              {id: 3, name: 'Pizza', calories: 777}
+        knex('foods').insert([{id: 1, name: 'Steak', calories: 500},
+                              {id: 2, name: 'Carrots', calories: 10},
+                              {id: 3, name: 'Pizza', calories: 777},
+                              {id: 4, name: 'Yogurt', calories: 50},
+                              {id: 5, name: 'Sesame Chicken', calories: 600},
+                              {id: 6, name: 'Eggs', calories: 200},
+                              {id: 7, name: 'Salad', calories: 100},
+                              {id: 8, name: 'Beans', calories: 250},
+                              {id: 9, name: 'Sandwich', calories: 450},
+                              {id: 10, name: 'Soup', calories: 300}
                               ], 'id')
-        .then(() => console.log('Seeding complete!'))
-        .catch(error => console.log(`Error seeding data: ${error}`))
+        .then(() => console.log('Seeding Foods complete!'))
+        .catch(error => console.log(`Error seeding Foods data: ${error}`))
       ])
     })
-    .catch(error => console.log(`Error seeding data: ${error}`));
+    .catch(error => console.log(`Error seeding Foods data: ${error}`));
 };
---
+++ db/seeds/dev/meals.js
@@ -0,0 +1,21 @@
+exports.seed = function(knex, Promise) {
+
+  return knex('meals').del() // delete all footnotes first
+    .then(() => {
+      return Promise.all([
+
+        knex('meals').insert([{id: 1, name: 'Breakfast', date: '2/1/19', calorie_goal: 400},
+                              {id: 2, name: 'Lunch', date: '2/1/19', calorie_goal: 700},
+                              {id: 3, name: 'Dinner', date: '2/1/19', calorie_goal: 800},
+                              {id: 4, name: 'Snack', date: '2/1/19', calorie_goal: 100},
+                              {id: 5, name: 'Breakfast', date: '2/2/19', calorie_goal: 400},
+                              {id: 6, name: 'Lunch', date: '2/2/19', calorie_goal: 700},
+                              {id: 7, name: 'Dinner', date: '2/2/19', calorie_goal: 800},
+                              {id: 8, name: 'Snack', date: '2/2/19', calorie_goal: 100}
+                              ], 'id')
+        .then(() => console.log('Seeding Meals complete!'))
+        .catch(error => console.log(`Error seeding Meals data: ${error}`))
+      ])
+    })
+    .catch(error => console.log(`Error seeding Meals data: ${error}`));
+};
